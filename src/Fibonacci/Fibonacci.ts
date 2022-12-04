8433ac8d719af238a7d2ade147a7253ecf2ce5b0
vmdharan <vmdharan@users.noreply.github.com>
2022-12-05 00:23:20 +1100

    Implement Fibonacci algorithm
    Implement Fibonacci algorithm.

---
+++ src/Fibonacci/Fibonacci.ts
@@ -0,0 +1,12 @@
+// Fibonacci algorithm
+// F(0) = 0; F(1) = 1
+// F(n) = F(n-1) + F(n-2), for n > 1
+
+const Fibonacci = (n: number): number => {
+    if(n <= 1) {
+        return n;
+    }
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+};
+
+export default Fibonacci;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { calculatePrimes, displayPrimes} from './Primes/sieveOfEratosthenes';
+import Fibonacci from './Fibonacci/Fibonacci';
+import { calculatePrimes, displayPrimes} from './Primes/SieveOfEratosthenes';
 
 console.log('math-algorithms');
 
 console.log('Primes less than 100');
 displayPrimes(calculatePrimes(100));
+
+console.log('Fibonacci, F(7)');
+console.log(Fibonacci(7));
